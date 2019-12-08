21ae1c901b1b4360002d7fc691fb79ab56d0022f
Nazarii Trokhymchenko <TROHYMCHENKO@GMAIL.COM>
2019-12-08 12:36:24 +0200

    Added pagination to the workouts checklist page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,7 +2,7 @@ class WorkoutsController < ApplicationController
   before_action :find_workout, only: [:show, :edit, :update, :destroy]
 
   def index
-    @workouts = Workout.all.order("created_at DESC")
+    @workouts = Workout.paginate(:page => params[:page], :per_page=>10)
   end
 
   def show
