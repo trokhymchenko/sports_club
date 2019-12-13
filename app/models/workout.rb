25d69eb7ad1159d88d2a5b2450e67b39d069a8c7
Nazarii Trokhymchenko <TROHYMCHENKO@GMAIL.COM>
2019-12-13 15:13:45 +0200

    Created index and show for exercises and updated associations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,9 +2,10 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :exercise_workouts
   has_many :exercises, through: :exercise_workouts
-  
+
   validates :title, presence: true, length: { maximum: 40 }
   validates :description, presence: true
+  validates :user_id, presence: true
   validates :question_title, presence: true, length: { minimum: 12, maximum: 40 }
   validates :question_description, presence: true
   validates_uniqueness_of :title
